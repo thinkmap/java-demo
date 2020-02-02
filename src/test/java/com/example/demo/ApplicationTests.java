0bde5042fb7f594f6964b27164a2a0aac56859f6
砧木 <zhenmu.zyl@alibaba-inc.com>
2020-02-02 13:05:49 +0800

    add test case1

---
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ApplicationTests {
+
+	@Test
+	public void testcase1() {
+		assertThat(1, is(1));
+	}
+
+}
