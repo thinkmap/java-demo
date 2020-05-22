acdfda9499e274a3a15e451d2baff4153c501a3e
rdc-rdc <liqiang.clq1@alibaba-inc.com>
2020-05-22 11:24:16 +0800

    update to v3

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! V2";
+        return "Greetings from Spring Boot! V3";
     }
 }
