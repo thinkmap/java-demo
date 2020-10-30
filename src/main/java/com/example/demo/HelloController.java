93e4a45dd19b8ea49adef60b89e092a2e92dca4a
砧木 <zhenmu.zyl@alibaba-inc.com>
2020-10-30 11:48:04 +0800

    update

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 import com.example.demo.model.ExampleData;
+import com.example.demo.util.XinBaoUtil;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.xml.crypto.Data;
 
 @RestController
 public class HelloController {
@@ -10,7 +13,7 @@ public class HelloController {
     @RequestMapping("/msg")
     public ExampleData msg() {
         ExampleData hello = ExampleData.builder().msg("hello").build();
-        System.out.println(hello.getMsg());
+        new XinBaoUtil().test(hello);
         return hello;
     }
 
---
+++ src/main/java/com/example/demo/util/XinBaoUtil.java
@@ -0,0 +1,12 @@
+package com.example.demo.util;
+
+import com.example.demo.model.ExampleData;
+
+/**
+ * @author yunlong
+ */
+public class XinBaoUtil {
+    public String test(ExampleData data) {
+        return data.getMsg();
+    }
+}
